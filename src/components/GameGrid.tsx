0728368af015ee15cd58afba05255c3b82bbad64
Aditya Gupta <aditya.gupta@beehyv.com>
2024-07-24 18:03:50 +0530

    Show Loading Skeletons

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,23 +1,29 @@
-
-import { SimpleGrid, Text } from '@chakra-ui/react'
-import useGames from '../hooks/useGames'
-import GameCard from './GameCard';
-
+import { SimpleGrid, Text } from "@chakra-ui/react";
+import useGames from "../hooks/useGames";
+import GameCard from "./GameCard";
+import GameCardSkeleton from "./GameCardSkeleton";
 
 const GameGrid = () => {
-
-    const {games,error}=useGames();
+  const { games, error, isLoading } = useGames();
+  const skeletons=[1,2,3,4,5,6]
 
   return (
     <>
-    {error && <Text>{error}</Text>}
-    <ul>
-        <SimpleGrid columns={{sm:1, md:2, lg:3, xl:5}} padding='10px' spacing='10px'>
-          {games.map(game=><GameCard game={game}></GameCard>)}
+      {error && <Text>{error}</Text>}
+      <ul>
+        <SimpleGrid
+          columns={{ sm: 1, md: 2, lg: 3, xl: 5 }}
+          padding="10px"
+          spacing="10px"
+        >
+          {isLoading && skeletons.map(skeleton=><GameCardSkeleton key={skeleton} />)}
+          {games.map((game) => (
+            <GameCard game={game}></GameCard>
+          ))}
         </SimpleGrid>
-    </ul>
+      </ul>
     </>
-  )
-}
+  );
+};
 
-export default GameGrid
+export default GameGrid;
