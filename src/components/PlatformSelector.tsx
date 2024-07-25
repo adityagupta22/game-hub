239c0912d50f64fe840b13cc4c13cc858ae7747e
Aditya Gupta <aditya.gupta@beehyv.com>
2024-07-25 16:40:32 +0530

    Build Sort Selector

--- src/components/PlatformSelector.tsx
+++ src/components/PlatformSelector.tsx
@@ -1,27 +1,35 @@
-import { Button, Menu, MenuButton, MenuItem, MenuList } from "@chakra-ui/react"
-import { BsChevronDown } from "react-icons/bs"
-import usePlatforms from "../hooks/usePlatforms"
-import { Platform } from "../hooks/useGames"
+import { Button, Menu, MenuButton, MenuItem, MenuList } from "@chakra-ui/react";
+import { BsChevronDown } from "react-icons/bs";
+import usePlatforms from "../hooks/usePlatforms";
+import { Platform } from "../hooks/useGames";
 
-interface Props{
-    onSelectPlatform:(platform:Platform)=>void
-    selectedPlatform:Platform | null
+interface Props {
+  onSelectPlatform: (platform: Platform) => void;
+  selectedPlatform: Platform | null;
 }
 
-const PlatformSelector = ({onSelectPlatform,selectedPlatform}:Props) => {
+const PlatformSelector = ({ onSelectPlatform, selectedPlatform }: Props) => {
+  const { data: platforms, error } = usePlatforms();
 
-    const {data:platforms,error,isLoading}=usePlatforms()
-    
-    if(error) return null
+  if (error) return null;
 
-    return (
-        <Menu>
-        <MenuButton as={Button} rightIcon={<BsChevronDown />}>{selectedPlatform?selectedPlatform.name:'Platforms'}</MenuButton>
-        <MenuList>
-            {platforms.map(platform=><MenuItem onClick={()=>onSelectPlatform(platform)} key={platform.id}>{platform.name}</MenuItem>)}
-        </MenuList>
+  return (
+    <Menu>
+      <MenuButton as={Button} rightIcon={<BsChevronDown />}>
+        {selectedPlatform ? selectedPlatform.name : "Platforms"}
+      </MenuButton>
+      <MenuList>
+        {platforms.map((platform) => (
+          <MenuItem
+            onClick={() => onSelectPlatform(platform)}
+            key={platform.id}
+          >
+            {platform.name}
+          </MenuItem>
+        ))}
+      </MenuList>
     </Menu>
-  )
-}
+  );
+};
 
-export default PlatformSelector
+export default PlatformSelector;
