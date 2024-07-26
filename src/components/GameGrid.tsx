cdb46f881874f494eff5a07e57e94f68f41d5862
Aditya Gupta <aditya.gupta@beehyv.com>
2024-07-26 13:01:02 +0530

    Refactor GameGrid

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -5,30 +5,36 @@ import GameCardSkeleton from "./GameCardSkeleton";
 import GameCardContainer from "./GameCardContainer";
 import { GameQuery } from "../App";
 
-interface Props{
-  gameQuery:GameQuery
+interface Props {
+  gameQuery: GameQuery;
 }
 
-const GameGrid = ({gameQuery}:Props) => {
-  const { data:games, error, isLoading } = useGames(gameQuery);
-  const skeletons=[1,2,3,4,5,6]
+const GameGrid = ({ gameQuery }: Props) => {
+  const { data: games, error, isLoading } = useGames(gameQuery);
+  const skeletons = [1, 2, 3, 4, 5, 6];
+
+  if (error) return <Text>{error}</Text>;
 
   return (
-    <>
-      {error && <Text>{error}</Text>}
-      <ul>
-        <SimpleGrid
-          columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
-          padding="10px"
-          spacing={6}
-        >
-          {isLoading && skeletons.map(skeleton=><GameCardContainer key={skeleton}><GameCardSkeleton  /></GameCardContainer>)}
-          {games.map((game) => (
-            <GameCardContainer key={game.id}><GameCard game={game}></GameCard></GameCardContainer>
+    <ul>
+      <SimpleGrid
+        columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
+        padding="10px"
+        spacing={6}
+      >
+        {isLoading &&
+          skeletons.map((skeleton) => (
+            <GameCardContainer key={skeleton}>
+              <GameCardSkeleton />
+            </GameCardContainer>
           ))}
-        </SimpleGrid>
-      </ul>
-    </>
+        {games.map((game) => (
+          <GameCardContainer key={game.id}>
+            <GameCard game={game}></GameCard>
+          </GameCardContainer>
+        ))}
+      </SimpleGrid>
+    </ul>
   );
 };
 
